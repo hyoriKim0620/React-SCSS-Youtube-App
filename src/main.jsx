898e5c1671718fbe1fc9d5f9adebada8b1d0318e
hyoriKim0620 <jenny7983@naver.com>
2023-12-19 21:57:34 +0900

    second commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import { BrowserRouter } from 'react-router-dom';
-import './scss/main.scss';
-import { SearchContextProvider } from './context/SearchContext';
-import { SideBarContextProvider } from './context/SideBarContext';
+/* eslint-disable no-undef */
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import "./scss/main.scss";
+import { SearchContextProvider } from "./context/SearchContext";
+import { SideBarContextProvider } from "./context/SideBarContext";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <SearchContextProvider>
         <SideBarContextProvider>
-        <App />
+          <App />
         </SideBarContextProvider>
       </SearchContextProvider>
     </BrowserRouter>
   </React.StrictMode>
-)
+);
