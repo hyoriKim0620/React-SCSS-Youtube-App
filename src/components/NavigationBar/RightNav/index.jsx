e13a22bb5a41fb6755e9668cc2bd30125cf0f1ed
hyoriKim0620 <jenny7983@naver.com>
2023-12-19 19:55:14 +0900

    first commit

---
+++ src/components/NavigationBar/RightNav/index.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { MdApps, MdNotifications, MdVideoCall } from "react-icons/md";
+import { Tooltip } from "react-tooltip";
+
+const RightNav = () => {
+  return (
+    <div className="buttons">
+      <button className="icon-container">
+        <MdVideoCall size={25} data-tip="Create" data-for="navbar" />
+      </button>
+      <button className="icon-container">
+        <MdApps size={25} />
+      </button>
+      <button className="icon-container">
+        <MdNotifications size={25} />
+      </button>
+
+      <Tooltip
+        id="navbar"
+        backgroundColor="gray"
+        effect="solid"
+        delayHide={150}
+        arrowColor="transparent"
+      />
+    </div>
+  );
+};
+
+export default RightNav;
---
+++ src/components/NavigationBar/index.jsx
@@ -0,0 +1,85 @@
+import React, { useContext } from "react";
+import LeftNav from "./LeftNav/index";
+import SearchBar from "./SearchBar/index";
+import RightNav from "./RightNav/index";
+import { BiArrowBack } from "react-icons/bi";
+import { ImSearch } from "react-icons/im";
+import { MdKeyboardVoice } from "react-icons/md";
+import { SearchContext } from "../../context/SearchContext";
+import useWindowSize from "../../helpers/useWindowSize";
+import { useNavigate } from "react-router-dom";
+import axios from "../../api/axios";
+
+const NavigationBar = () => {
+  const navigate = useNavigate();
+  const { width } = useWindowSize();
+  const {
+    searchQuery,
+    setSearchQuery,
+    showSpecialSearchBar,
+    setShowSpecialSearchBar,
+  } = useContext(SearchContext);
+
+  const handleChange = (e) => {
+    setSearchQuery({
+      ...searchQuery,
+      input: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (searchQuery.input !== "") {
+      const response = await axios.get(
+        `/search?part=snippet&maxResults=10&q=${searchQuery.input}`
+      );
+
+      setSearchQuery({
+        ...searchQuery,
+        videos: response.data.items,
+      });
+
+      navigate(`/results/${searchQuery.input}`);
+    }
+  };
+
+  const specialSearchBarRender = (
+    <div className="special_searchbar">
+      <button onClick={() => setShowSpecialSearchBar(false)}>
+        <BiArrowBack size={25} />
+      </button>
+      <form onSubmit={handleSubmit}>
+        <input
+          value={searchQuery.input}
+          onChange={handleChange}
+          type="text"
+          name="search"
+          placeholder="Search"
+        />
+        <button>
+          <ImSearch size={20} />
+        </button>
+      </form>
+      <button className="icon-container voiceIcon">
+        <MdKeyboardVoice size={25} />
+      </button>
+    </div>
+  );
+
+  return (
+    <nav className="Navbar">
+      {width <= 640 && showSpecialSearchBar ? (
+        specialSearchBarRender
+      ) : (
+        <>
+          <LeftNav />
+          <SearchBar onChange={handleChange} onSubmit={handleSubmit} />
+          <RightNav />
+        </>
+      )}
+    </nav>
+  );
+};
+
+export default NavigationBar;
